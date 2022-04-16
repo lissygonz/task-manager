83d845557446996cae1608ca9e5919eacef8a19c
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-04-16 14:43:20 -0400

    refactored

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,25 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
-
   event.preventDefault();
-
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  //check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+}
+formEl.reset();
+//reset form fields for next task to be entered
+document.querySelector("input[name='task-name]").value="";
+document.querySelector("select[name='task-type']").selectedIndex = 0;
 
   //package up data as an object
 
   var taskDataObj ={
     name: taskNameInput,
     type: taskTypeInput
-  
 };
 
 // send it as an arugement to createTaskEl
@@ -21,13 +28,6 @@ var taskFormHandler = function (event) {
 createTaskEl(taskDataObj);
 
 };
-//check if input values are empty strings
-if (!taskNameInput || !taskTypeInput) {
-  alert("You need to fill out the task form!");
-  return false;
-}
-
-formEl.reset();
 
 //taskDataObj
 var createTaskEl = function(taskDataObj) {
@@ -40,8 +40,9 @@ listItemEl.className = "task-item";
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
 taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
 listItemEl.appendChild(taskInfoEl);
+
+console.dir(listItemEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
